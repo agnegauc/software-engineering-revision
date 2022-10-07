4ed310ce0b05d0c6f95f1f232513a96be2e87c6c
Jonas Girdzijauskas <jonas.girdzijauskas@gmail.com>
2022-10-07 14:06:39 +0300

    krakend working example

--- KrakenD-API-Gateway/README.md
+++ KrakenD-API-Gateway/README.md
@@ -1,1 +1,4 @@
-If starting KrakenD up in Windows doesn't work, replace "$PWD:" with "." in the "start" script from package.json.
+# Simple KrakenD API gateway. 
+Make sure to configure it according to the official documentation. This project serves as an example of a working prototype.
+
+KrakenD needs no programming as it offers a declarative way to create the endpoints. It is well structured and layered, and open to extending its functionality using plug-and-play middleware developed by the community or in-house.
--- KrakenD-API-Gateway/app.ts
+++ KrakenD-API-Gateway/app.ts
@@ -4,7 +4,7 @@ import { dateRouter } from "./routes/date/date.router";
 import { greetingRouter } from "./routes/greeting/greeting.router";
 import { numberRouter } from "./routes/number/number.router";
 
-const PORT = 3_001;
+const PORT = 3001;
 
 const app = express();
 
@@ -15,5 +15,8 @@ app.use("/date", dateRouter);
 app.use("/number", numberRouter);
 app.use("/greeting", greetingRouter);
 
-// https://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
-app.listen(PORT, "YOUR_IP", () => console.log(`Listening to port ${PORT}.`));
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}.`);
+});
+
+export default app;
