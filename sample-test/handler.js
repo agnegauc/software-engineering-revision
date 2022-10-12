b5a9dcef2051d388c3c7a9f4cca74be91d5bced8
Jonas Girdzijauskas <jonas.girdzijauskas@gmail.com>
2022-10-12 15:30:49 +0300

    improved code quality

--- sample-test/handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const greeting = require("./greeting/greeting").greeting;
-const hello = require("./hello/hello").hello;
-
-module.exports.greeting = greeting;
-
-module.exports.hello = hello;
---
+++ serverless-node-api/handler.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const { greeting } = require("./greeting/greeting");
+const { hello } = require("./hello/hello");
+
+module.exports.greeting = greeting;
+
+module.exports.hello = hello;
