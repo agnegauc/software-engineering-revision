d2e395f83b359c918222e52aca8b6d135d8aba78
Jonas Girdzijauskas <jonas.girdzijauskas@gmail.com>
2022-10-13 09:48:01 +0300

    remove unused code

--- KrakenD-API-Gateway/app.ts
+++ KrakenD-API-Gateway/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { dateRouter } from "./routes/date/date.router";
 import { greetingRouter } from "./routes/greeting/greeting.router";
-import { userRouter } from "./routes/user.router";
 import { numberRouter } from "./routes/number/number.router";
 
 const PORT = 3001;
@@ -15,8 +14,6 @@ app.use(express.json());
 app.use("/date", dateRouter);
 app.use("/number", numberRouter);
 app.use("/greeting", greetingRouter);
-
-app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}.`);
